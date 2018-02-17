792fc6a92af99640e7a4121b8b4544a4bc617f5b
JustinZhengBC <justinzhengbc@gmail.com>
2018-02-17 10:33:02 -0800

    Base

---
+++ UBCSprouts/DocumentsView/DocumentsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DocumentsViewController.swift
+//  UBCSprouts
+//
+//  Created by Justin Zheng on 2018-02-17.
+//  Copyright © 2018 Justin Zheng. All rights reserved.
+//
+
+import UIKit
+
+class DocumentsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
