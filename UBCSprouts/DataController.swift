4bfaa6e55a79628a6c5340fe84b8142e5b397f3c
Raghav Thakur <raghav.thakur.rt@gmail.com>
2018-03-26 18:13:24 -0700

    Created DataController

---
+++ UBCSprouts/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  UBCSprouts
+//
+//  Created by Raghav Thakur on 2018-03-26.
+//  Copyright © 2018 Justin Zheng. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class DataController: NSObject {
+    var managedObjectContext: NSManagedObjectContext
+    init(completionClosure: @escaping () -> ()) {
+        persistentContainer = NSPersistentContainer(name: "DataModel")
+        persistentContainer.loadPersistentStores() { (description, error) in
+            if let error = error {
+                fatalError("Failed to load Core Data stack: \(error)")
+            }
+            completionClosure()
+        }
+    }
+}
