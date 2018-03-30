f8c4ba4313ba3d1a6a196096dd567aafe353c088
JustinZhengBC <justinzhengbc@gmail.com>
2018-03-29 21:07:38 -0700

    Merged the storyboard and project files so everything is visible

--- UBCSprouts/VolunteerView/Event.swift
+++ UBCSprouts/VolunteerView/Event.swift
@@ -5,17 +5,18 @@
 //  Created by Justin Zheng on 2018-03-01.
 //  Copyright © 2018 Justin Zheng. All rights reserved.
 //
-
 import Foundation
 
 struct Event {
     
     var name: String!
+    var location: String!
     var description: String!
     var id: Int!
     
-    init(called name: String, describedAs description: String, withID id: Int) {
+    init(called name: String, atLocation location: String, describedAs description: String, withID id: Int) {
         self.name = name
+        self.location = location
         self.description = description
         self.id = id
     }
